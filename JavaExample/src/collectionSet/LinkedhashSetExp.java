6ae4ab5db00abe9e2fbe24b172563e626330573d
smuvva <srinivasarao_muvva@yahoo.com>
2016-11-29 14:47:07 -0600

    LinkedHashset

---
+++ JavaExample/src/collectionSet/LinkedhashSetExp.java
@@ -0,0 +1,22 @@
+package collectionSet;
+
+
+import java.util.LinkedHashSet;
+
+public class LinkedhashSetExp {
+
+	public static void main(String[] args) {
+		LinkedHashSet h= new LinkedHashSet();
+		h.add("B");
+		h.add("C");
+		h.add("D");
+		h.add("Z");
+		h.add(null);
+		h.add(10);
+		System.out.println(h.add("Z")); //false because duplicate are not allowed.
+		System.out.println(h);
+	}
+
+	
+
+}
