5b7a9abdc9b941f4d32e16814b59f4ea8066b3ed
smuvva <srinivasarao_muvva@yahoo.com>
2016-11-30 19:02:43 -0600

    forLoop

---
+++ JavaExample/src/loops/forLoopExample1.java
@@ -0,0 +1,28 @@
+package loops;
+
+public class forLoopExample1 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		  char Grade;
+		  
+		  Grade='D';
+
+		  
+		  if (Grade=='A') {
+			  System.out.println("A grade ");
+		  } else if (Grade=='B') {
+			  System.out.println("B grade ");
+		  }
+		  else if (Grade=='C') {
+			  System.out.println("C grade ");
+		  }
+		  else  {
+			  System.out.println("D grade ");
+		  }
+
+			}
+
+}
