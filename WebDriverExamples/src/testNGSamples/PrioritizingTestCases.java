d17f58d719ea8fa95dd6c1f5a3831b71c8ef7308
smuvva <srinivasarao_muvva@yahoo.com>
2016-12-01 13:10:06 -0600

    TESTNG

--- WebDriverExamples/src/testNGSamples/PrioritizingTestCases.java
+++ WebDriverExamples/src/testNGSamples/PrioritizingTestCases.java
@@ -3,7 +3,37 @@ package testNGSamples;
 import org.testng.annotations.Test;
 
 public class PrioritizingTestCases {
-  @Test
-  public void f() {
-  }
+  
+	
+	@Test
+	 
+	  public void One() {
+	 
+	      System.out.println("This is the Test Case number One ");
+	 
+	  }
+	 
+	  @Test
+	 
+	  public void Two() {
+	 
+		  System.out.println("This is the Test Case number Two ");
+	 
+	  }
+	 
+	  @Test
+	 
+	  public void Three() {
+	 
+		  System.out.println("This is the Test Case number Three");
+	 
+	  }
+	 
+	  @Test
+	 
+	  public void Four() {
+	 
+		  System.out.println("This is the Test Case number Four");
+	 
+	  }
 }
