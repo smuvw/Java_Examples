68489f7da44c44bee94415001a944e4c8e64a0f2
smuvva <srinivasarao_muvva@yahoo.com>
2016-12-02 12:55:06 -0600

    TESTNG

---
+++ WebDriverExamples/src/testNGSamples/TestNGGroup.java
@@ -0,0 +1,24 @@
+package testNGSamples;
+
+import org.testng.annotations.Test;
+
+public class TestNGGroup {
+	
+	@Test (priority=1,groups="high")
+	public void one(){
+		System.out.println("This is test one");
+	}
+
+	
+	@Test (priority=2,groups="low")
+	public void two(){
+		System.out.println("This is test two");
+	}
+	
+	 
+	@Test (priority=2,groups="high")
+	public void three(){
+		System.out.println("This is test three");
+	}
+	
+}
