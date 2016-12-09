60baa7c7de9e7e756a33e35671b8df93f4a30952
SMuvva <SMuvva@192.168.222.1>
2016-12-08 20:01:16 -0600

    Assignment

---
+++ JavaExample/src/operators/Assignment.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		
+		int c=20;
+		int d=5;
+		
+		
+		System.out.println(" c=c+d value is  " + (c+=d));
+		System.out.println("c=c-d value is  " +  (c-=d));
+
+	}
+
+}
