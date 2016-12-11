39a39781fbb150f51ef5e194eb1b0d66068258e3
SMuvva <SMuvva@VKodali-Lap8.4YourSoul.net>
2016-12-11 10:27:04 -0600

    hub and node

--- Grid/src/testSample1.java
+++ Grid/src/testSample1.java
@@ -6,6 +6,17 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
+
+/*Hub :
+java -jar selenium-server-standalone-2.53.0.jar -role hub
+
+Node:
+Java -Dwebdriver.chrome.driver=C:\Softwares\chromedriver.exe -Dwebdriver.ie.driver=C:\Softwares\IEDriverServer.exee  -jar selenium-server-standalone-2.53.0.jar -role webdriver -hub http://localhost:4444/grid/register -port 5555 -browser browserName=firefox,maxInstances=4 -browser browserName=iexplore -browser browserName=chrome,maxInstances=3 -maxSession 4
+
+*
+*
+*/
+
 public class testSample1 {
 	
 	@Test
