f8ef5e29c1f495b9061686f1e921cf9017f37cad
SMuvva <SMuvva@VKodali-Lap8.4YourSoul.net>
2016-12-11 10:00:36 -0600

    TESTNG

---
+++ Grid/src/testSample1.java
@@ -0,0 +1,33 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class testSample1 {
+	
+	@Test
+	public void testlogin() throws MalformedURLException{
+		
+		DesiredCapabilities cap= DesiredCapabilities.firefox();
+		
+		cap.setBrowserName("firefox");
+		
+		
+		
+		cap.setPlatform(Platform.ANY);
+		
+				
+		
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+			
+		driver.get("https://www.facebook.com/");
+
+			
+	driver.quit();
+		
+	}
+
+}
