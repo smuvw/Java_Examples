0176b67fa03a77c4d6f7e13254732a4e74198bf7
SMuvva <SMuvva@VKodali-Lap8.4YourSoul.net>
2016-12-12 11:42:57 -0600

    Exceptions

--- JavaExample/src/exceptions/throwsExample3.java
+++ JavaExample/src/exceptions/throwsExample3.java
@@ -2,7 +2,7 @@ package exceptions;
 
 public class throwsExample3 extends RuntimeException {
 	
-	// if we are not extends with RuntimeException then we will get compile time error
+	// If we are not extends with RuntimeException then we will get compile time error
 	
 	public void m1() throws throwsExample3  {
 		
