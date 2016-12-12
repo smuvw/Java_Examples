f72951cceb5375eeeb5b9150390ef93c73125fab
SMuvva <SMuvva@VKodali-Lap8.4YourSoul.net>
2016-12-12 11:41:40 -0600

    Exception

--- JavaExample/src/exceptions/throwsExample2.java
+++ JavaExample/src/exceptions/throwsExample2.java
@@ -7,13 +7,13 @@ public class throwsExample2 {
           dostuff();
 	}
 	
-	public static void dostuff() throws InterruptedException{
+	public static void dostuff() throws InterruptedException {
 		
 		domorestuff();
 		
 	}
 	
-	public static void domorestuff() throws InterruptedException{
+	public static void domorestuff() throws InterruptedException {
 		
 		Thread.sleep(1000);
 	}
---
+++ JavaExample/src/exceptions/throwsExample3.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+public class throwsExample3 extends RuntimeException {
+	
+	// if we are not extends with RuntimeException then we will get compile time error
+	
+	public void m1() throws throwsExample3  {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
