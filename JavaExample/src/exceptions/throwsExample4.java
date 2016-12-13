d58673f9bc8dbffa6df1bfa6a3ccd9dfa3744551
SMuvva <SMuvva@VKodali-Lap8.4YourSoul.net>
2016-12-12 20:41:14 -0600

    throws

---
+++ JavaExample/src/exceptions/throwsExample4.java
@@ -0,0 +1,17 @@
+package exceptions;
+
+public class throwsExample4 {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			System.out.println("hello");
+		} catch(ArithmeticException e ){
+			
+			
+		}
+		
+	}
+
+}
---
+++ JavaExample/src/exceptions/throwsExample5.java
@@ -0,0 +1,48 @@
+package exceptions;
+
+import java.io.IOException;
+
+public class throwsExample5 {
+	
+public static void main(String[] args) {
+		
+		try {
+			
+			System.out.println("hello");
+		} catch(Exception e ){
+			
+			
+		}
+		
+		
+		/*	try {
+			
+			System.out.println("hello");
+		} catch(IOException e ){
+			
+			
+		}*/
+		
+		
+		/*try {
+			
+			System.out.println("hello");
+		} catch(InterruptedException e ){
+			
+			
+		}
+		*/
+		
+		
+		try {
+			
+			System.out.println("hello");
+		} catch(Error e ){
+			
+			
+		}
+		
+		
+	}
+
+}
