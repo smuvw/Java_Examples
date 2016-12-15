dc493638584056972df0a9a40e00576d5bfe59ea
SMuvva <SMuvva@VKodali-Lap8.4YourSoul.net>
2016-12-14 20:40:38 -0600

    throw

--- JavaExample/src/exceptions/throwExample2.java
+++ JavaExample/src/exceptions/throwExample2.java
@@ -3,6 +3,7 @@ package exceptions;
 public class throwExample2 {
 
 	//Programmer  is creating Exception object explicitly  and hangover to the JVM manually  
+	//Results is same as  default exception handling  result
 	
 	public static void main(String[] args) {
 		
