4f46b4e7d051364f6edcbf287e7abdd409240e0c
SMuvva <SMuvva@VKodali-Lap8.4YourSoul.net>
2016-12-15 17:02:47 -0600

    throw

--- JavaExample/src/exceptions/throwExample2.java
+++ JavaExample/src/exceptions/throwExample2.java
@@ -5,12 +5,23 @@ public class throwExample2 {
 	//Programmer  is creating Exception object explicitly  and hangover to the JVM manually  
 	//Results is same as  default exception handling  result
 	
-	public static void main(String[] args) {
+	public static void validate (int age) {
 		
-		System.out.println(10/0);
-		
-		throw new ArithmeticException( "/by zero");
+		if(age<18) 
+	      throw new ArithmeticException("not valid"); 
+			else
+				System.out.println("welove to vote ");
+		}
 
-	}
+		public static void main(String[] args) {
+			
+			validate(13);
+			
+			/*int age=13;
+			if(age<18) 
+			      throw new ArithmeticException("not valid"); 
+					else
+						System.out.println("welove to vote ");*/
 
+		}
 }
