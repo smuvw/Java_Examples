dbb9ae6d897a0738583e86694ca97671ed4652e7
SMuvva <SMuvva@192.168.222.1>
2017-02-23 05:42:46 -0600

    Wait

--- WebDriverExamples/src/waits/ExplicitWaitExample.java
+++ WebDriverExamples/src/waits/ExplicitWaitExample.java
@@ -1,10 +1,14 @@
 package waits;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ExplicitWaitExample {
@@ -14,12 +18,16 @@ public class ExplicitWaitExample {
 		ChromeDriver driver=new ChromeDriver();
 		
 		//WebDriverWait wait = new WebDriverWait(WebDriverRefrence,TimeOut);
-		WebDriverWait wait =new WebDriverWait(driver,20);
+		WebDriverWait wait =new WebDriverWait(driver,20L);
 		
 		driver.get("https://jqueryui.com");
 		driver.manage().window().maximize();
 	    wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(".//*[@id='sidebar']/aside[1]/ul/li[3]/a")));
 	    System.out.println("welcome");
+	    
+	
+	    
+	    
 	}
 
 }
