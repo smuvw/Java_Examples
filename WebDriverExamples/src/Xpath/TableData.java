973e3bdd9fa584964f0b8e7137fa15cececf6946
Administrator <Administrator@192.168.222.1>
2017-04-13 16:09:42 -0500

    xpath

---
+++ WebDriverExamples/src/Xpath/TableData.java
@@ -0,0 +1,33 @@
+package Xpath;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TableData {
+
+	public static void main(String[] args) {
+  ChromeDriver driver= new ChromeDriver();
+		
+		driver.get("http://www.cricbuzz.com/live-cricket-scorecard/18130/mi-vs-srh-10th-match-indian-premier-league-2017");
+		
+		
+		int rownum=6;
+		int colnum=4;
+		
+		String xp_start=".//*[@id='innings_1']/div[1]/div[";
+		String xp_mid="]/div[";
+		String xp_end="]";
+		
+		for (int rows=3;rows<=rownum;rows++){
+			
+			for(int cols=1;cols<=colnum;cols++){
+				
+				String name= driver.findElementByXPath(xp_start+rows+xp_mid+cols+xp_end).getText();
+				System.out.println(name);
+			}
+		}
+				
+		
+
+	}
+
+}
